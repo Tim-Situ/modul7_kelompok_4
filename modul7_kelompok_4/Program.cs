c4818c7cb7c9e92f704ea7407eba9cbffa4baea3
Muhammad Haulul Azkiyaa <haululazkiya@gmail.com>
2024-04-04 10:43:04 +0700

    menambahkan class TeamMembers1302223007

--- modul7_kelompok_4/Program.cs
+++ modul7_kelompok_4/Program.cs
@@ -6,5 +6,10 @@ internal class Program
     {
         DataMahasiswa1302223007 mhs1302223007 = new DataMahasiswa1302223007();
         mhs1302223007.ReadJSON();
+
+        Console.WriteLine("");
+
+        TeamMembers1302223007 team1302223007 = new TeamMembers1302223007();
+        team1302223007.ReadJSON();
     }
 }
---
+++ modul7_kelompok_4/TeamMembers1302223007.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.Json;
+
+namespace modul7_kelompok_4
+{
+    public class TeamMembers1302223007
+    {
+        public class Members
+        {
+            public String firstName { get; set; }
+            public String lastName { get; set; }
+            public String gender { get; set; }
+            public int age { get; set; }
+            public String nim { get; set; }
+        }
+
+        public List<Members> members { get; set; }
+
+        public void ReadJSON()
+        {
+            string filepath = "../../../jurnal7_2_1302223007.json";
+            // membaca dari json
+            string jsonString = File.ReadAllText(filepath);
+            // deserialize dari json menjadi object
+            TeamMembers1302223007 mhs = JsonSerializer.Deserialize<TeamMembers1302223007>(jsonString);
+
+            Console.WriteLine("Team member list: ");
+            int i = 0;
+            foreach (var member in mhs.members)
+            {
+                Console.WriteLine(member.nim + " " + member.firstName + " " + member.lastName + " " +
+                    "(" + member.age + " " + member.gender + ")");
+                i++;
+            }
+        }
+    }
+}
+
