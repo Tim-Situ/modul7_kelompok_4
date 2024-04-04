6ac619b6292f4b1a5793cad9958f15cb7d226c49
Fauzein Mulya Warman <fauzeinmulyawarman@Fauzeins-MacBook-Pro.local>
2024-04-04 10:16:08 +0700

    menambahkan class DataMahasiswa1302223127

---
+++ modul7_kelompok_4/DataMahasiswa1302223127.cs
@@ -0,0 +1,47 @@
+﻿using System.Text.Json;
+
+namespace modul7_kelompok_4
+{
+	public class DataMahasiswa1302223127
+	{
+		public class Address
+		{
+			public string streetAddress { get; set; }
+			public string city { set; get; }
+			public string state { set; get; }
+
+        }
+
+		public class Course
+		{
+			public string code { get; set; }
+			public string name { get; set; }
+		}
+
+		public string firstName { set; get; }
+        public string lastName { set; get; }
+        public string gender { set; get; }
+        public int age { set; get; }
+        public Address address { set; get; }
+        public List<Course> courses { set; get; }
+
+		public void ReadJSON()
+		{
+			string fileJson = File.ReadAllText("../../../jurnal7_1_1302223127.json");
+			DataMahasiswa1302223127 dataMahasiswa = JsonSerializer.Deserialize<DataMahasiswa1302223127>(fileJson);
+
+			Console.WriteLine("=== Data Mahasiswa ===");
+			Console.WriteLine($"Nama : {dataMahasiswa.firstName} {dataMahasiswa.lastName}");
+			Console.WriteLine($"Jenis Kelamin : {dataMahasiswa.gender}");
+			Console.WriteLine($"Umur : {dataMahasiswa.age}");
+			Console.WriteLine($"Alamat : Jln. {dataMahasiswa.address.streetAddress}, {dataMahasiswa.address.city}, {dataMahasiswa.address.state}");
+			Console.WriteLine("Mata Kuliah : ");
+
+			foreach(var course in dataMahasiswa.courses)
+			{
+				Console.WriteLine($"- ({course.code}) {course.name}");
+			}
+		}
+	}
+}
+
